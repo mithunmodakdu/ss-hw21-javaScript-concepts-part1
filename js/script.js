76b7142144ff3733ed54db930f733dfddf2cb6a0
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2024-08-11 07:55:25 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// example1
+function showText() {
+    document.getElementById('s_text').innerHTML="Hello SSB";
+}
+
+// example2
+function showText2(){
+    document.getElementById('s_text2').innerHTML="I am Mithun Kumer Modak";
+}
+
+// example3
+function showText3(){
+    document.getElementById('s_text3').innerHTML="Lorem ipsum dolor sit amet.";
+}
+
+// example4
+function showText4(){
+    document.getElementById('s_text4').innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, rem?";
+}
+
+// example5
+function showText5(){
+    document.getElementById('s_text5').innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit modi delectus, quod recusandae ipsum eum.";
+}
+
+// example6
+function showText6(){
+    document.getElementById('s_text6').innerHTML="Lorem ipsum dolor sit amet.";
+}
+
+// example7
+function showText7(){
+    document.getElementById('s_text7').innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing.";
+}
+
+// example8
+function showText8(){
+    document.getElementById('s_text8').innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit.";
+}
+
+// example9
+function showText9(){
+    document.getElementById('s_text9').innerHTML="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit.";
+}
+
+// example10
+function showText10(){
+    document.getElementById('s_text10').innerHTML="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, fugit.";
+}
